bd5441f10fcec87b9808b7a1784bb294d21e3053
Aboubacar Diawara <aboubacardiawara909@gmail.com>
2024-02-26 01:06:59 +0100

    more readable method

--- server/handlers/restaurants.handler.go
+++ server/handlers/restaurants.handler.go
@@ -9,6 +9,17 @@ import (
 )
 
 func RestaurantsHandler(c *gin.Context) {
+	radius, latitude, longitude, notDone := parseCoordinates(c)
+	if notDone {
+		return
+	}
+
+	restaurants := services.RestaurantsAround(longitude, latitude, radius)
+
+	c.JSON(http.StatusOK, gin.H{"restaurants": restaurants})
+}
+
+func parseCoordinates(c *gin.Context) (float64, float64, float64, bool) {
 	latStr := c.Param("lal")
 	longStr := c.Param("long")
 	radiusAsStr := c.Param("radius")
@@ -19,22 +30,19 @@ func RestaurantsHandler(c *gin.Context) {
 	radius, err := strconv.ParseFloat(radiusAsStr, 64)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"success": 0, "message": "Invalid radius"})
-		return
+		return 0, 0, 0, true
 	}
 	latitude, err := strconv.ParseFloat(latStr, 64)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"success": 0, "message": "Invalid altitude"})
-		return
+		return 0, 0, 0, true
 	}
 
 	longitude, err := strconv.ParseFloat(longStr, 64)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"success": 0, "message": "Invalid longitude"})
-		return
+		return 0, 0, 0, true
 
 	}
-
-	restaurants := services.RestaurantsAround(longitude, latitude, radius)
-
-	c.JSON(http.StatusOK, gin.H{"restaurants": restaurants})
+	return radius, latitude, longitude, false
 }
